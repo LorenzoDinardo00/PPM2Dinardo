0f19da34b51921672ec6af388cc25f9ae624f6e0
lorenzo <lorenzo.dinardo@stud.unifi.it>
2024-06-18 18:44:40 +0200

    deploy9

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name="admin"),
+    path('admin', admin.site.urls, name="admin"),
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts", views.AllPostsView.as_view(), name="posts-page"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
